ebc1d1b48af4316aa3ee33ca8b2112f177ce7982
BaoLocPham <phambaoloc163@gmail.com>
2023-07-16 00:07:57 +0700

    [FIX] test_proper_boundaries

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -29,6 +29,9 @@ def go(args):
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx]
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Saving")
     df.to_csv("clean_sample.csv", index=False)
 
